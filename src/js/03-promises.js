d9876d7b58ca1bf46e9b69b8cbb5b705ce632253
RonnieBk <veronicabrvk00@gmail.com>
2023-09-29 15:09:24 +0200

    Finish task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const delayInput = form.elements.delay;
+const stepInput = form.elements.step;
+const amountInput = form.elements.amount;
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let delayNext = delayInput.value;
+  for (let i = 1; i <= amountInput.value; i++) {
+    createPromise(i, delayNext)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delayNext = Number(delayNext) + Number(stepInput.value);
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({
+          position,
+          delay,
+        });
+      } else {
+        reject({
+          position,
+          delay,
+        });
+      }
+    }, delay);
+  });
 }
